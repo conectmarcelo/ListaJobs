2f004b963c812cf00c577ba0e4762cb268f69325
conectmarcelo <conectmarcelo@hotmail.com>
2022-07-03 15:38:46 -0300

    remove

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,7 +6,13 @@
       :rows="jobs"
       :columns="columns"
       row-key="name"
-    />
+    >
+    <template v-slot:body-cell-actions="props">
+      <q-td :props="props">
+        <q-btn icon="delete" color="negative" dense size="md" @click="deleteJob(props.row.id)"/>
+      </q-td>
+    </template>
+    </q-table>
   </q-page>
 </template>
 
@@ -14,20 +20,23 @@
 
 import { defineComponent, ref, onMounted } from 'vue'
 import jobsService from 'src/services/jobs.js'
-
+import { useQuasar } from 'quasar'
 export default defineComponent({
   name: 'IndexPage',
 
   setup () {
     const jobs = ref([])
-    const { list } = jobsService()
+    const { list, remove } = jobsService()
 
     const columns = [
       { name: 'id', label: 'ID', align: 'left', field: 'id', sortable: true },
       { name: 'descricao', label: 'Descrição', align: 'left', field: 'descricao', sortable: true },
       { name: 'dtMaxConclusao', label: 'Data Máxima de conclusão', align: 'left', field: 'dtMaxConclusao', sortable: true },
-      { name: 'tempoEstimado', label: 'Tempo estimado', align: 'left', field: 'tempoEstimado', sortable: true }
+      { name: 'tempoEstimado', label: 'Tempo estimado', align: 'left', field: 'tempoEstimado', sortable: true },
+      { name: 'actions', label: 'Ações', align: 'right', field: 'actions' }
     ]
+
+    const $q = useQuasar()
 
     onMounted(() => {
       getJobs()
@@ -42,9 +51,27 @@ export default defineComponent({
       }
     }
 
+    const deleteJob = async (id) => {
+      try {
+        $q.dialog({
+          title: 'Remover',
+          message: 'Deseja remover este job ?',
+          cancel: true,
+          persistent: true
+        }).onOk(async () => {
+          await remove(id)
+          $q.notify({ message: 'Apagado com sucesso!', icon: 'check', color: 'positive' })
+          await getJobs()
+        })
+      } catch (error) {
+        $q.notify({ message: 'Erro ao apagar', icon: 'times', color: 'positive' })
+      }
+    }
+
     return {
       jobs,
-      columns
+      columns,
+      deleteJob
     }
   }
 })
